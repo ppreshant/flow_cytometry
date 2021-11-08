65685a6c87b3a8eee47d78d139b61487d6ceb2cc
ppreshant <ppresh.ant@gmail.com>
2021-11-07 18:16:51 -0600

    2-get_number_of_datasets : complicated function to return the number of datasets

---
+++ scripts_general_fns/2-get_number_of_datasets_fcs.R
@@ -0,0 +1,37 @@
+
+get_number_of_datasets_fcs <- function(input_file_path) # provide the path of the FCS file to retrieve the number of datasets in it
+
+{ # source : https://support.biofl_connectionductor.org/p/9135360/#9135370
+  
+  # get file fl_connectionnection where 'filename' is some local file with multiple data segments
+  fl_connection <- file(input_file_path, open = 'rb')
+  
+  ## relevant code taken from findOffsets as just calling the function itself doesn't return the right info
+  offsets <- flowCore:::readFCSheader(fl_connection)
+  offsets <- matrix(offsets, nrow = 1, dimnames = list(NULL, names(offsets)))
+  txt <- flowCore:::readFCStext(fl_connection, offsets[1, ], emptyValue = FALSE)
+  
+  addOff <- 0
+  
+  if("$NEXTDATA" %in% names(txt)) {
+    nd <- as.numeric(txt[["$NEXTDATA"]]) } else {nd <- 0}
+  
+  txt.list <- list(txt)
+  i <- 1
+  
+  while(nd != 0) {
+    i <- i + 1
+    addOff <- addOff + nd
+    offsets <- rbind(offsets, flowCore:::readFCSheader(fl_connection, addOff))
+    this.txt <- flowCore:::readFCStext(fl_connection, offsets[nrow(offsets),], emptyValue = FALSE)
+    nd <- as.numeric(this.txt[["$NEXTDATA"]])
+    txt.list[[i]] <- this.txt
+  }
+  
+  # close the file connection
+  close(fl.connection)
+  
+  # return number of data sets
+  nDataset <- length(txt.list)
+  
+}

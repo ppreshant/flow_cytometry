f75e9f61b41766acb85cfbaefd728881c198f574
ppreshant <ppresh.ant@gmail.com>
2021-11-07 23:09:33 -0600

    reading_fcs : Moved libraries into general file

--- reading_fcs.R
+++ reading_fcs.R
@@ -9,10 +9,7 @@ subfolder_name <- 'Marielles cyanostuff/'
 file.name_input <- 'Marielle cyanostuff_2021-10-01_at_02-56-43pm' # input file name without .fcs
 
 # Prelims ----
-library(tidyverse)  # general data handline
-library(flowCore) # flow cytometry data types library
-library(flopr)  # flow cytometry specific library
-library(ggcyto) # plotting package for flow cytometry data
+source('./0-general_functions_fcs.R') # call the function to load libraries and auxilliary functions
 
 
 fl.path = str_c(folder_data, subfolder_name, file.name_input, '.fcs')
@@ -29,7 +26,7 @@ fl.path = str_c(folder_data, subfolder_name, file.name_input, '.fcs')
 fl.set <- read_multidata_fcs(fl.path,
                           directory_path = str_c(folder_data, subfolder_name, file.name_input))
 
-fcs.subset <- Subset(fl.set, 'G01.fcs')
+# fcs.subset <- Subset(fl.set, 'G01.fcs')
 
 # Plotting ----
 
@@ -39,10 +36,10 @@ fcs.subset <- Subset(fl.set, 'G01.fcs')
 # Plot density
 pltden <- ggcyto(fl.set, aes(x = 'FSC-HLin')) + 
   geom_density(fill = 'blue', alpha = 0.3) + 
-  scale_x_logicle()
+  scale_x_logicle() # some bi-axial transformation for FACS (linear near 0, logscale at higher values)
 
 # plot scatterplots
 ggcyto(fl.set, aes(x = 'FSC-HLin', y = 'SSC-HLin')) + 
   # geom_point(alpha = 0.1) + 
-  geom_hex()
+  geom_hex(bins = 64) + 
   scale_x_logicle() + scale_y_logicle()

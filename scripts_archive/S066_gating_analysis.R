160e6d4019da8e570fe8cc8af00fe29c78c77e51
ppreshant <ppresh.ant@gmail.com>
2023-04-06 22:37:31 -0500

    Extending S066 analysis to all days - wonderful graphics worth the 4.5 h of effort!

--- scripts_archive/S066_gating_analysis.R
+++ scripts_archive/S066_gating_analysis.R
@@ -19,14 +19,88 @@ source('scripts_general_fns/13-formatting_plot_funs.R')
 
 
 # Load data ----
-title_name <- 'S066x_Ara dose-1-processed'
+flnms <- c('S066x_Ara dose-1', 'S066b1_Ara dose_ d2-d4', 'S066b2_Ara dose d5')
+title_name <- 'S066-processed' # appears on fig title and saved plot name
 
-counts_gated <- str_c('FACS_analysis/tabular_outputs/', title_name, '-gated_counts.csv') %>% read.csv
+counts_gated <- 
+  flnms %>% 
+  {str_c('FACS_analysis/tabular_outputs/', ., '-processed-gated_counts.csv')} %>% 
+  
+  map_dfr(read.csv) %>% # read all the gated counts files
+  mutate(Arabinose = as.numeric(assay_variable), .before = assay_variable) %>% # make Ara numeric
+  mutate(day = str_remove(sample_category, '^d'), .before = sample_category) # make a new column for day
 
+unique_counts <- summarise(counts_gated, mean_freq = mean(freq),
+                           .by = c(Arabinose, assay_variable, sample_category, day, Population))
+
+
+# Plots ----
+
+# plot dose-response
+# save_jitter <- position_jitter(width = .5, height = 0)
+
+plt_dose <-
+{ggplot(counts_gated,
+        aes(Arabinose, freq, colour = sample_category)) +
+    
+    geom_point() + 
+    geom_line(aes(y = mean_freq), unique_counts) + 
+    
+    guides(colour = guide_legend(title = 'day'))} %>% 
+  
+  format_logscale_x() + 
+  
+  ggtitle('ON state fraction', subtitle = title_name)
+
+
+plt_controls <- 
+  
+  {filter(counts_gated, str_detect(assay_variable, 'glu|OFF|ON')) %>% # plot non Ara samples
+  
+  ggplot(aes(assay_variable, freq, colour = sample_category)) +
+      
+      geom_point(show.legend = F) + 
+      ylab(NULL) + xlab('Controls')} %>% 
+  print
+
+# Assemble plots
+library(patchwork)
+
+full_plot <- 
+plt_dose + plt_controls + 
+  plot_layout(guides = 'collect', widths = c(4, 1))
+
+ggsave(plot_as(title_name, '-dose_response'), full_plot, width = 6, height = 4)
+
+
+# Plot timecourse 
+plt_timecourse <-
+  {ggplot(counts_gated,
+          aes(day, freq, label = biological_replicates)) +
+      
+      geom_point(size = 1, position = position_jitter(width = 0.2, height = 0)) + 
+      
+      # join each replicate      
+      geom_line(aes(group = interaction(assay_variable, biological_replicates)), alpha = 0.2) + 
+      
+      # Line through mean
+      # geom_line(aes(y = mean_freq, label = NULL, 
+      #               group = assay_variable), unique_counts) +
+      
+      facet_wrap(~ assay_variable) +
+      
+      ggtitle('ON state fraction', subtitle = title_name)} %>% 
+  print
+    
+ggsave(plot_as(title_name, '-timecourse-repl'), plt_timecourse, width = 8, height = 7)
+
+plotly::ggplotly(plt_timecourse)
+
+# Day 0 plot ----
 
 # look at d0 fractions only
-d0_counts <- filter(counts_gated, sample_category == 'd0') %>% 
-  mutate(Arabinose = as.numeric(assay_variable), .before = assay_variable)
+d0_counts <- filter(counts_gated, sample_category == 'd0') 
+  
 
 d0_unique <- summarise(ungroup(d0_counts), mean_freq = mean(freq),
                        .by = c(Arabinose, Population))

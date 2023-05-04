fae629a35f1168b981723ee7ca50801c54e00ba0
ppreshant <ppresh.ant@gmail.com>
2023-05-04 16:42:52 -0500

    adding switch to change the y axis variable for combined dataset - regular data : .yvar : assay_variable, facets : other_category/no facets - combined data : .yvar : data_set, facets : assay_variable - Adjusted plot save sizes appropriately : height = nvars x 1.2 ; width = ncats x 3 - tested to work for S050 full dataset!

--- scripts_general_fns/17-plot_ridges_fluor.R
+++ scripts_general_fns/17-plot_ridges_fluor.R
@@ -3,10 +3,14 @@
 #' wrapper for plotting ridgeline plots with some user controls
 #' Optional showing of median lines and text of median values
 #' Automatically detects ordering variable `list_of_ordered_levels`
-
+ 
 plot_ridges_fluor <- function(.show_medians = TRUE, # shows median lines and text labels 
                               .facets_other_category = FALSE, .save_plots = TRUE)
 {
+  
+  # yaxis variable ----
+  # variable name to plot on y axis. Typically assay_variable. Use "data_set" for combined data
+  .yvar <- if(combined_data) expr(data_set) else expr(assay_variable)
   
   # plot dimensions ----
   # only for other_category stuff
@@ -14,6 +18,12 @@ plot_ridges_fluor <- function(.show_medians = TRUE, # shows median lines and tex
   if(.facets_other_category)
   {# determine the fig width based on # of facets
   n_facets_plt = pull(fcsunique.subset, other_category) %>% unique() %>% length()
+  plt_height = est_plt_side/n_facets_plt
+  
+  } else if(combined_data) {
+    n_facets_plt = pull(fcsunique.subset, assay_variable) %>% unique %>% length
+    plt_height = pull(fcsunique.subset, data_set) %>% unique %>% length * 1.2
+    
   }
   
   
@@ -28,29 +38,37 @@ plot_ridges_fluor <- function(.show_medians = TRUE, # shows median lines and tex
         ) +
           
           # conditional plotting of ridges : if ordering exists or not
-          {if (exists('list_of_ordered_levels')) {
-            ggridges::geom_density_ridges(aes
-                                          (y = fct_relevel(assay_variable, 
-                                                           list_of_ordered_levels[['assay_variable']])), 
-                                          alpha = 0.3,
-                                          
-                                          
-                                          # adding mean/median lines
-                                          # source: https://datavizpyr.com/add-mean-line-to-ridgeline-plot-in-r-with-ggridges/
-                                          
-                                          quantile_lines = .show_medians, # to show median
-                                          quantiles = 2 # check if works when quantile_lines is F
-                                          # quantile_fun = function(x, ...) median(x) # make a function to calculate median
-            )
+          ggridges::geom_density_ridges(
+            aes(
+              
+              y = if(exists('list_of_ordered_levels') && !combined_data) 
+              
+                {fct_relevel(assay_variable, 
+                           list_of_ordered_levels[['assay_variable']]) 
+              
+                } else {{.yvar}}), 
+              
+              alpha = 0.3,
+              
+              # adding mean/median lines
+              # source: https://datavizpyr.com/add-mean-line-to-ridgeline-plot-in-r-with-ggridges/
+              
+              quantile_lines = .show_medians, # to show median
+              quantiles = 2 # check if works when quantile_lines is F
+              # quantile_fun = function(x, ...) median(x) # make a function to calculate median
+          ) +
             
-          } else ggridges::geom_density_ridges(aes(y = assay_variable), alpha = 0.3) } + # TODO : put quantile lines here
-          
           
           # Add facets for other_category or remove default facets (default = each sample in a facet)
-          {if(.facets_other_category) {
+          {if(.facets_other_category) { # facets for other category
             facet_wrap(facets = vars(other_category), ncol = n_facets_plt, # control facets
                        scales = 'free_y') 
-          } else facet_wrap(facets = NULL)} + # control facets
+          
+            } else if(combined_data) { # facets for assay_variable
+              facet_wrap(facets = vars(assay_variable), ncol = n_facets_plt, # control facets
+                         scales = 'free_y')
+            
+            }  else facet_wrap(facets = NULL)} + # No facets
           
           
           scale_x_logicle() +  # some bi-axial transformation for FACS (linear near 0, logscale at higher values)
@@ -60,7 +78,7 @@ plot_ridges_fluor <- function(.show_medians = TRUE, # shows median lines and tex
           # Labels of the median line
           {if (exists('fcsunique.subset') & .show_medians) { # only if the labelling subset data exists
             geom_text(data = filter(fcsunique.subset, Fluorophore == .x),
-                      mapping = aes(x = mean_medians, y = assay_variable,
+                      mapping = aes(x = mean_medians, y = {{.yvar}},
                                     label = round(mean_medians, 0)),
                       nudge_y = -0.1) } } + 
           # median text is imperfect : showing the mean of the medians of 3 replicates. Refer to link for better alternative
@@ -73,7 +91,6 @@ plot_ridges_fluor <- function(.show_medians = TRUE, # shows median lines and tex
   # CAVEAT : The median values shown on the chart are slightly different from the lines 
   # Text: (mean of median/replicate) ; lines :  (median/combined distribution?)
   # TODO : generalize to plot all fluorophores on different charts? 
-  # TODO : put the subsetting + ridge plots into a two functions for easily calling adhoc
   
   # plotting limited axis ranges
   # plt_trunc_ridges <- plt_ridges + ggcyto_par_set(limits = list(x = c(10, 3e5))) # arbitrary axis limits for log10 scale
@@ -82,7 +99,7 @@ plot_ridges_fluor <- function(.show_medians = TRUE, # shows median lines and tex
   
   if(.save_plots){ # save plots unless user input prevents this
     
-    if(.facets_other_category) { # saving plots if faceted by other_category
+    if(.facets_other_category | combined_data) { # saving plots if faceted by other_category/data_set
       map(names(fluor_chnls), # iterate over fluorescence channels
           
           ~ ggsave(str_c('FACS_analysis/plots/', 
@@ -90,7 +107,7 @@ plot_ridges_fluor <- function(.show_medians = TRUE, # shows median lines and tex
                          '-', .x, 
                          '.png'),
                    plot = plt_ridges[[.x]], # plt_ridges
-                   height = est_plt_side/n_facets_plt, width = 3 * n_facets_plt) # use automatic estimate for plt sides : 2 / panel
+                   height = plt_height, width = 3 * n_facets_plt) # use automatic estimate for plt sides : 2 / panel
       )
       
       
@@ -100,13 +117,15 @@ plot_ridges_fluor <- function(.show_medians = TRUE, # shows median lines and tex
           
           ~ ggsave(str_c('FACS_analysis/plots/', 
                          title_name,  # title_name, 
-                         '-ridge', '-', .x, 
+                         '-', .x, 
                          '.png'),
                    plot = plt_ridges[[.x]], # plt_ridges
                    height = est_plt_side, width = 5) # use automatic estimate for plt sides : 2 / panel
       ) }
     # TODO : convert into a function? call for specific colour..
   }
+  
+  return(plt_ridges)
   
   
 }

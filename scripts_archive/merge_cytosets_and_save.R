88c17a5ba11f805ce668d9bcb1cb76b0eed20f1d
ppreshant <ppresh.ant@gmail.com>
2023-05-14 02:25:10 -0500

    running a vectorized flow to combine multiple datasets with optional manual renaming

--- scripts_archive/merge_cytosets_and_save.R
+++ scripts_archive/merge_cytosets_and_save.R
@@ -16,32 +16,36 @@ fcs_export_dir <- 'S071_combined/' # end with "_combined"
 # manual workflow ----
 # Use this to load files, manually change the pData (which goes into file renaming) before calling the saver
 
-# Load data 
-fl.set <- get_fcs_and_metadata(str_c(base_directory, folder_name, file.name_input))
-
-
-sample_cat_fill <- c('^U' = 'Uninduced', '^I' = 'Induced') # for filling abbreviations in sample_category
 
 # fix pData manually
-new_pdata2 <- pData(fl.set) %>% 
-  separate(sample_category, into = c('sample_category', 'dir_key'), sep = '-') %>% # add new category for directory
+manual_modify_pdata <- function(.pdata)
+{
   
-  mutate(across(dir_key, # fix d-1 trunc and make controls as d-1 
-                ~ replace_na(.x, 'd-1') %>% str_replace('d$', 'd-1') )) %>% 
+  # for filling abbreviations in sample_category
+  sample_cat_fill <- c('^U' = 'Uninduced', '^I' = 'Induced') 
   
-  # fill abbreviatios of induced and uninduced
-  mutate(across(sample_category, 
-                ~ str_replace_all(.x, sample_cat_fill)))
+  new_pdata2 <- .pdata %>% 
+    
+    # separate day from `U-d0` into a `dir_key` column
+    separate(sample_category, into = c('sample_category', 'dir_key'), sep = '-') %>% # add new category for directory
+    
+    mutate(across(dir_key, # fix d-1 trunc and make controls as d-1 
+                  ~ replace_na(.x, 'd-1') %>% str_replace('d$', 'd-1') )) %>% 
+    
+    
+    # fill abbreviations of induced and uninduced
+    mutate(across(sample_category, 
+                  ~ str_replace_all(.x, sample_cat_fill))) 
+  
+  # TODO : include optional calculations for the other variable, for facetting S066-67 data
+}
 
+# for single dir : Load data, modify and write : Uncomment to use
+# fl.set <- get_fcs_and_metadata(str_c(base_directory, folder_name, file.name_input),
+#                                manually_modify_pdata = T, # needs a function `manual_modify_pdata` -- above
+#                                rename_and_save_fcs = T)
 
-pData(fl.set) <- new_pdata2 # replace the pData
-
-
-rename_fcs_and_save(.flset = fl.set, interactive_session = TRUE)
-
-
-
-# vectorized merger ----
+# Vectorized merger ----
 # iterate through the list of folders and put them all into the export folder specified above
 
 
@@ -49,30 +53,49 @@ rename_fcs_and_save(.flset = fl.set, interactive_session = TRUE)
 # Get directories ----
 
 # recognize directories with a regex pattern for expt name
-subdirs <- dir(base_directory, '^S071._')
-# not used yet.. attach to make dir.paths
+subdirs <- dir(base_directory, '^S071._') %>% 
+  str_c('/') # add the trailing slash to the directory
 
-# manual subdirectory building # Geared for S050 ; but edit as necessary (one time use anyway right?) 
-days <- 1:8 # -1:8 # User inputs
-
-# Make a list of folders
-dir.paths = map(days, ~ str_c(base_directory, 'S050_d', .x, '/'))
+# dir.paths <- str_c(base_directory, subdirs)
 
 
-# General rename-save ----
+# manual subdirectory building (uncomment to use)
+
+# Geared for S050 ; but edit as necessary (one time use anyway right?) 
+# days <- 1:8 # -1:8 # User inputs
+# 
+# dir.paths = map(days, ~ str_c(base_directory, 'S050_d', .x, '/')) # Make a list of folders
+
+
+
+# Modify-rename-save ----
 # with same metadata for all directories
 
-# get metadata -- only once for all data/ else use .get_metadata = T below
-sample_metadata <- get_and_parse_plate_layout(folder_name) 
+# get metadata -- enable if using only once for all data/ else use .get_metadata = T below
+# sample_metadata <- get_and_parse_plate_layout(folder_name) 
 
 # vectorized to work in all of the dir paths loaded above
 
-source('scripts_general_fns/20-load_fcs_and_save_renamed.R')
-dir.paths %>% map(get_fcs_and_metadata, .get_metadata = F,
-                  rename_and_save_fcs = T, .interactive_session = F)
-  
-# work in progress ; vectorize this over dir.paths now..
+# rm(folder_name) # remove this from the global environment to prevent confusion
+# subdirs %>%
+#   map( \(x) 
+#        {folder_name <- x ; dir.path <- str_c(base_directory, x) ; # set assignments
+#          get_fcs_and_metadata(.dirpath = dir.path,
+#                               
+#                               # other inputs to the function
+#                               .get_metadata = F,
+#                               manually_modify_pdata = T, # needs a function `manual_modify_pdata` -- above
+#                               
+#                               rename_and_save_fcs = T, .interactive_session = T)} )
 
+dir.paths %>% map(get_fcs_and_metadata,
+                  manually_modify_pdata = T, # needs a function `manual_modify_pdata` -- above
+                  rename_and_save_fcs = T, .interactive_session = T)
+
+
+# for same metadata workflow : S050
+# dir.paths %>% map(get_fcs_and_metadata, .get_metadata = F,
+#                   rename_and_save_fcs = T, .interactive_session = F)
 
 
 # Old script -----

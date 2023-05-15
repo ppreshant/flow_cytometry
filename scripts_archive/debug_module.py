8d214680af2a746a7a2db6a595b01e2352de3cb6
ppreshant <ppresh.ant@gmail.com>
2023-05-15 16:49:50 -0500

    trying to test a debg module - doesn't work

--- scripts_archive/debug_module.py
+++ scripts_archive/debug_module.py
@@ -13,3 +13,13 @@ def scope_tester():
     
     x = globals()['x']
     print('/n', x)
+    
+def debug_tester():
+    # Doesn't work:( try calling this from a notebook or console and see if it stops at breakpoints
+    print('enterted debug tester module')
+    
+    # place debugger here
+    a = 45
+    
+    print(a)
+    print('exiting module')

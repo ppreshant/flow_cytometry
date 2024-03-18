58f66b3a2bfd71f61d9c974406614276f629f2f0
ppreshant <ppresh.ant@gmail.com>
2024-03-18 03:05:51 -0500

    work in progress to calculate flowworkspace with a function - Think if this is useful in more than just 2 places (main function and 20-subset data..)

---
+++ scripts_archive/23-get_flowworkspace_summary.R
@@ -0,0 +1,8 @@
+# 23-get_flowworkspace_summary.R
+
+#' Saving summary statistics (mean, median, quartiles etc.) of each fluorophore in fcs collection
+#' 
+
+# Move line 60 onwards from `analyze_fcs.R` here. Starting from 
+
+# get summary statistics

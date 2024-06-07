5f249db53f9ab2ef9ddaf813059919ce47e9bd95
ppreshant <ppresh.ant@gmail.com>
2024-06-07 12:18:15 -0500

    documenting parameters for future; plot all WTs - gating summary plot: work in progress

--- scripts_archive/S089_adhoc_plots.R
+++ scripts_archive/S089_adhoc_plots.R
@@ -32,3 +32,48 @@ ggsave(str_c('FACS_analysis/plots/',
              '-ridges-by-organism', fl_suffix, 
              '.png'),
        height = 6, width = 20) # change height and width by number of panels
+
+
+# gating analysis ----
+
+## plot all the WTs -----
+
+fcsunique.subset <- 
+  subset_cytoset(fl.set,
+                 
+                 # optional manual filtering (additional to above)
+                 custom_filtering = str_detect(assay_variable, 'WT$') # only WT samples
+  )
+
+source('scripts_general_fns/17-plot_ridges_fluor.R') # source script
+
+# make ridgeline plot. Save with a different suffix
+plt_ridges_wt <- 
+  plot_ridges_fluor(.show_medians = show_medians, 
+                    .plot_filename = 'S089_WT') 
+
+# facet by sample category
+plt_ridges_wt$green + 
+  facet_wrap(facets = vars(sample_category), scales = 'free_y', ncol = 1) + # facet by organism (sample_category)
+  theme(legend.position = 'none') # remove legend/redundant
+
+# save plot (overwrite)
+ggsave(str_c('FACS_analysis/plots/', 
+             'S089_WT-green', # overwrite above plot
+             '.png'),
+       height = 6, width = 5) # change height and width by number of panels
+
+
+## gating ----
+
+# Choose to gate by Pi : H07 in a (S089)
+sampleNames(fl.set) %>% .[str_detect(., 'H07')] # check sample names
+
+## plotting gated counts ----
+facet_by_sample_category <- TRUE
+
+
+# run 11-manual_gating_workflow with these parameters 
+# skip the analysis.R sourcing in the script (since it was already run)
+
+subset_this_file <- 'Pi_H07' # subset the data to this file
